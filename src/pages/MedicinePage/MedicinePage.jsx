70ced29a4f27e508620542482938466403d10060
Volodymyr892 <sevcukvolodimir896@gmail.com>
2025-03-07 19:43:08 +0200

    added files component Filters

--- src/pages/MedicinePage/MedicinePage.jsx
+++ src/pages/MedicinePage/MedicinePage.jsx
@@ -1,9 +1,11 @@
+// import Filters from "../../components/Filters/Filters";
 import MedicineList from "../../components/MedicineList/MedicineList";
 import css from "./MedicinePage.module.css"
 
 export default function MedicinePage() {
   return (
     <main className={css.container}>
+      {/* <Filters/> */}
       <MedicineList/>
     </main>
   )
