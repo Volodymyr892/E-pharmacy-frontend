033695f0f2aa4b4eca2cf27a470c2d9e740859c5
Volodymyr892 <sevcukvolodimir896@gmail.com>
2025-03-29 22:05:23 +0200

    added modal header

---
+++ src/components/BurgerModal/BurgerModal.jsx
@@ -0,0 +1,10 @@
+import css from "./BurgerModal.module.css";
+
+export default function BurgerModal() {
+    return(
+        <div className={css.container}>
+modal
+        </div>
+    )
+}
+
--- src/components/Header/Haader.jsx
+++ src/components/Header/Haader.jsx
@@ -1,9 +1,12 @@
 import css from "./Header.module.css"
 import logo from "../../assets/logo.svg"
+import BurgerModal from "../BurgerModal/BurgerModal"
 export default function Header(){
     return(
         <div className={css.header}>
             <img src={logo} alt="" />
+
+            <BurgerModal/>
         </div>
     )
 }
